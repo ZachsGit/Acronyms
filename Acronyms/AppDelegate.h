6ba149d8959cf043846bf6f7677cf68b630422cc
Zach <zachzhx@gmail.com>
2017-03-16 13:56:08 -0700

    Initial Commit

---
+++ Acronyms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Acronyms
+//
+//  Created by Zhang Xu on 3/16/17.
+//  Copyright © 2017 Zhang Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Acronyms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Acronyms
+//
+//  Created by Zhang Xu on 3/16/17.
+//  Copyright © 2017 Zhang Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
